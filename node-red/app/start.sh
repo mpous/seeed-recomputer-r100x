97968558777e0edd839757a537b0aa4fe5909513
Marc Pous <marc.pous@gmail.com>
2024-07-26 14:48:38 +0200

    fixing ttyAMA5 issues in the node-red service

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -2,6 +2,16 @@
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
+# Start RS485_de from Seeed Studio
+echo "Starting rs485_DE instances.........................." 
+
+cd /usr/local/bin
+./rs485_DE /dev/ttyAMA2 /dev/gpiochip0 6 /dev/ttyAMA10 /dev/gpiochip2 12 & 
+./rs485_DE /dev/ttyAMA5 /dev/gpiochip0 24 /dev/ttyAMA12 & 
+./rs485_DE /dev/ttyAMA3 /dev/gpiochip0 17 /dev/ttyAMA11 & 
+
+echo "rs485_DE instances started..........................."
+
 
 # Make sure ssh known hosts file is present
 if ! test -f /data/known_hosts;
